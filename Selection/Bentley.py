d80379c346540e7b730adad8779a011a2c2d0570
Ben Lambert <ben.c.lambert@gmail.com>
2018-12-23 00:34:18 +0000

    Stylistic changes

--- Selection/Bentley.py
+++ Selection/Bentley.py
@@ -1,40 +1,52 @@
 import random
 import numpy
 import pandas
+import collections
+
 
 class Individual:
-    def __init__(self,aNumber):
+    def __init__(self, aNumber):
         self.number = aNumber
+
     def getNumber(self):
         return(self.number)
+
 
 class Population:
     def __init__(self):
         self.members = []
         self.max = 0
-    def addIndividual(self,aIndividual):
+
+    def addIndividual(self, aIndividual):
         self.members.append(aIndividual)
         aMax = self.getMax()
         aNumber = aIndividual.getNumber()
         if aMax < aNumber:
             self.setMax(aNumber)
+
     def getIndividuals(self):
         return self.members
+
     def getNumbers(self):
         lNumber = []
         for people in self.members:
             lNumber.append(people.getNumber())
         return lNumber
+
     def getPopSize(self):
         return len(self.members)
-    def setMax(self,aMax):
+
+    def setMax(self, aMax):
         self.max = aMax
+
     def getMax(self):
         return self.max
+
     def getNumberCount(self):
         lNumbers = pandas.Series(self.getNumbers())
         return lNumbers.value_counts()
-    def getVariantFreq(self,aNumber):
+
+    def getVariantFreq(self, aNumber):
         lTemp = self.getNumberCount()
         lTemp = lTemp.to_frame()
         lTemp['number'] = lTemp.index
@@ -42,16 +54,23 @@ class Population:
         aInd = lBool.to_frame().iloc[:, 0].sum()
         if aInd > 0:
             aValue = lTemp[lBool]
-            aFreq = aValue.iloc[0,0]
+            aFreq = aValue.iloc[0, 0]
         else:
             aFreq = 0
         return aFreq
+
     def getMeanVariantFreq(self):
         return (self.getNumberCount() / self.getPopSize()).mean()
-    def getAllFreq(self):
-        pass
 
-def createOffspring(aParent,aMu,aPopulation):
+    def getAllFreq(self):
+        cnt = collections.Counter()
+        for people in self.members:
+            a_number = people.getNumber()
+            cnt[a_number] += 1
+        return
+
+
+def createOffspring(aParent, aMu, aPopulation):
     if aMu > random.random():
         aMax = aPopulation.getMax()
         aNumber = aMax + 1
@@ -59,46 +78,51 @@ def createOffspring(aParent,aMu,aPopulation):
         aNumber = aParent.getNumber()
     return Individual(aNumber)
 
+
 def getUnique(lTemp):
     return list(set(lTemp))
 
-def createIndividual(aNumber,lAllPastAndPresentIndividuals):
+
+def createIndividual(aNumber, lAllPastAndPresentIndividuals):
     lAllPastAndPresentIndividuals.append(aNumber)
     return Individual(aNumber)
 
-def createInitialPopulation(aPopSize,aMax):
+
+def createInitialPopulation(aPopSize, aMax):
     aPopulation = Population()
-    for i in range(0,aPopSize):
-        aIndividual = Individual(random.randint(1,aMax))
+    for i in range(0, aPopSize):
+        aIndividual = Individual(random.randint(1, aMax))
         aPopulation.addIndividual(aIndividual)
     aPopulation.setMax(max(aPopulation.getNumbers()))
     return aPopulation
 
-def populationReproduce(aPopulation,aMu):
+
+def populationReproduce(aPopulation, aMu):
     bPopulation = Population()
     bPopulation.setMax(aPopulation.getMax())
     lParents = aPopulation.getIndividuals()
     aPopSize = aPopulation.getPopSize()
-    for i in range(0,aPopSize):
-        aRand = random.randint(0,aPopSize-1)
-        aIndividual = createOffspring(lParents[aRand],aMu,bPopulation)
+    for i in range(0, aPopSize):
+        aRand = random.randint(0, aPopSize-1)
+        aIndividual = createOffspring(lParents[aRand], aMu, bPopulation)
         bPopulation.addIndividual(aIndividual)
     return bPopulation
 
 
-def runAll(aNumGenerations,aPopSize,aMu,aMax):
+def runAll(aNumGenerations, aPopSize, aMu, aMax):
     lPops = []
-    aInitialPopulation = createInitialPopulation(aPopSize,aMax)
+    aInitialPopulation = createInitialPopulation(aPopSize, aMax)
     lPops.append(aInitialPopulation)
     lExisting = list(set(aInitialPopulation.getNumbers()))
-    for generations in range(1,aNumGenerations):
+    for generations in range(1, aNumGenerations):
         # print(generations)
         aParents = lPops[generations-1]
         lExisting = lExisting + aParents.getNumbers()
         lExisting = list(set(lExisting))
-        aChildren = populationReproduce(aParents,aMu)
+        aChildren = populationReproduce(aParents, aMu)
         lPops.append(aChildren)
     return lPops
+
 
 def getArray(lPops):
     lNumbers = []
@@ -107,9 +131,11 @@ def getArray(lPops):
     lNumbers = numpy.array(lNumbers)
     return lNumbers
 
+
 def getFlatList(lPops):
     aArray = getArray(lPops)
     return aArray.flatten()
+
 
 def getCounts(lPops):
     lNumbers = getFlatList(lPops)
