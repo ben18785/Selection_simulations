42153af70ff4eb23614c2903e962337ef216e9d9
Ben Lambert <ben.c.lambert@gmail.com>
2018-12-23 14:56:40 +0000

    Update s_frequency_dependent_functional_form.R

--- s_frequency_dependent_functional_form.R
+++ s_frequency_dependent_functional_form.R
@@ -24,3 +24,12 @@ curve(logistic_probability(mu, -10, x), 0, 1)
 
 # positive frequency dependence -- EFFECT IS TINY!
 curve(logistic_probability(mu, 10, x), 0, 1)
+
+# relative selection in a given generation
+s <- function(aBeta, p_g1, p_g2){
+  return((1 + aBeta * p_g1) / (1 + aBeta * p_g2) - 1)
+}
+
+curve(s(1, x, 0.1), 0, 1)
+curve(s(-1, x, 0.1), 0, 1)
+
